334bc9b3b41c2dff7cb853884074bff0a9c74e59
bitsixfour <tugirasles@gmail.com>
2026-05-13 22:58:32 -0500

    url

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,10 @@ struct Args {
     #[arg(short, long, default_value_t = 1)]
     count: u8,
 }
+
+fn parse_url<'a>(args: &'a Args) -> &'a str {
+    &args.url
+}
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let usr = env::var("USER").unwrap_or_else(|_| "unknown".to_string());
@@ -27,6 +31,9 @@ async fn main() -> anyhow::Result<()> {
     let ffmpeg = lib.join("vids");
 
     let args = Args::parse();
+    let vid: &str = parse_url(&args);
+
+
     println!("Good morning saar....");
     if let Some(url) = args.name {
         j
